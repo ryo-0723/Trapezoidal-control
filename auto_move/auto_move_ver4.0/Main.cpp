57cbf58eac090fc3bd8991dd2f0c6a12c5388c35
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2022-12-08 03:32:17 +0900

    直線のものだけにした

--- auto_move/auto_move_ver4.0/Main.cpp
+++ auto_move/auto_move_ver4.0/Main.cpp
@@ -4,12 +4,11 @@
 
 void Main()
 {
-	double point1[2] = { 1.5,4.0 };
-	double point2[2] = {3.0,8.0};
-	double point3[2] = { 6.0,16.0 };
-	double now_point1[2] = {0,0};
+	double point1[2] = { 1.5,3.0 };
+	double point2[2] = { 3.0,5.0 };
+	double point3[2] = { 5.0,2.0 };
+	double now_point1[2] = { 0,0 };
 	int next_point=0;
-	int a = 0;
 	double tar = 0;
 	double mil = 0;
 	double get = 0;
@@ -17,8 +16,8 @@ void Main()
 	double mil2 = 0;
 	double get2 = 0;
 	unsigned long loop = 0;
-	Plotter plotter1, plotter2, plotter3, plotter4;
-	Trapezoidal auto_set(1.5,3.0);
+	Plotter plotter1, plotter2;
+	Trapezoidal auto_set(4.5,3.0);
 	while (System::Update())
 	{
 		if (auto_set.millis() > 3000) {
@@ -33,15 +32,17 @@ void Main()
 		switch (next_point) {
 		case 1:
 			auto_set.calculation(0.0, 1.0, point1, now_point1);
+			Print << U"aa";
 			break;
 		case 2:
-			auto_set.calculation(1.0, 3.0, point2, now_point1);
+			auto_set.calculation(1.0, 0.0, point2, now_point1);
 			break;//break入れろやああああああ
 		case 3:
-			auto_set.calculation(3.0, 0.0, point3, now_point1);
+			auto_set.calculation(0.0, 0.0, point3, now_point1);
 			break;
 		case 4:
 			auto_set.calculation(0.0, 0.0, point1, now_point1);
+			Print << U"aaaaaaaaaaaaaaaaaaaa";
 			break;
 		}
 
@@ -53,48 +54,25 @@ void Main()
 			get2 = (auto_set.tar_p_x() - tar2) / (((double)loop * 0.001) - mil);//m/s
 			tar = auto_set.tar_p_y();
 			tar2 = auto_set.tar_p_x();
-			mil = (double)loop / 1000.0;
+			mil = (double)loop * 0.001;
 		}
 
-
-
-
 		ClearPrint();
-		Print << mil;
-		Print << auto_set.all_time();
-		Print << auto_set.end_p_x();
-		Print << auto_set.end_p_y();
+		Print << auto_set.next_status();
 		Print << next_point;
 		Print << tar2;
 		Print << tar;
-		Print << auto_set.up__tmie();
+
 		plotter1
 			.resize(Scene::Rect())
-			.plot(get)
-			.maximum(5)
-			.minimum(-5)
-			.draw(Palette::Aqua)
-			.drawGrid();
-
-		plotter2
-			.resize(Scene::Rect())
-			.plot(get2)
-			.maximum(5)
-			.minimum(-5)
-			.draw(Palette::Red)
-			.drawGrid();
-
-		plotter3
-			.resize(Scene::Rect())
-			.plot(tar2)//x
+			.plot(get2)//x
 			.maximum(10)
 			.minimum(-10)
 			.draw(Palette::Green)
 			.drawGrid();
-
-		plotter4
+		plotter2
 			.resize(Scene::Rect())
-			.plot(tar)//y
+			.plot(get)//y
 			.maximum(10)
 			.minimum(-10)
 			.draw(Palette::Yellow)
--- auto_move/auto_move_ver4.0/Trapezoidal_control.h
+++ auto_move/auto_move_ver4.0/Trapezoidal_control.h
@@ -1,38 +1,28 @@
 ﻿#pragma once
 /*
 * 現状の課題点は次の順路に移行したときに、がたつきが見られるところ
-* ↑仕方ない気がしてる
-* あとすること
-*     円を描く動作を描くこと
-* 
-* 距離の積算方式はずれが大きいとのことで断念した
-* 代わりに毎周期距離を一から計算している
+* 円を描く動作を描くこと
+*
 */
 #include <Siv3D.hpp>
-#define DEG_TO_RAD  0.01745329251994329576923690768489L;
-#define RAD_TO_DEG  57.295779513082320876798154814105L;
-#define PI  3.141592653589793238462643383279502884L;
-
 class Trapezoidal {
 private:
-	double end_s = 0, start_s = 0, max_s = 0, limit_s = 0.00;
+	double end_s = 0, start_s = 0, max_s = 0, limit_s = 0;
 	double up_t = 0, down_t = 0, max_power_t = 0, all_t = 0;
 	double L1 = 0, L3 = 0;
-	double dir = 0, dis = 0;
+	double kakudo = 0, dir = 0, dis = 0;
 	double acc_, acc;
-	double target[2] = { 0,0 };
-	double end_potion[2] = { 0,0 };
+	double target[2] = {};
+	double end_potion[2] = {};
 	unsigned long times = 0;
 	bool Stop_flag = false, Reset_flag = false, Timer_state = false;
 	bool cal_state = false, next_state = true;
-
 public:
-	Trapezoidal(double acc, double max_s){
-		this->acc=acc;
+	Trapezoidal(double acc, double max_s) {
+		this->acc = acc;
 		this->max_s = max_s;
 		acc_ = 1.00 / acc;
 	}
-
 	unsigned long millis() {
 		return (unsigned long)Time::GetMillisec();
 	}
@@ -49,7 +39,6 @@ public:
 	double sq(double r) {
 		return r * r;
 	}
-
 	void timer_start() {
 		if (Timer_state == 0) {
 			times = millis();
@@ -59,7 +48,7 @@ public:
 		Reset_flag = 0;
 	}
 	void timer_stop() {
-		Timer_state=0;
+		Timer_state = 0;
 		if (Stop_flag == 0) {
 			times = millis() - times;
 		}
@@ -74,72 +63,60 @@ public:
 		return millis() - times;// ms
 	}
 
-	void calculation(double start_s, double end_s,double dis_[2],double now_p[2]) {
-		if (cal_state) 	return;
-		//switch文によって何度呼び出されても一度だけ動作するようにするもの
+	void calculation(double start_s, double end_s, double dis_[2], double now_p[2]) {
+		timer_start();//タイマーを開始する←何度呼び出しても一度だけ実行される
+
+		if (cal_state) return;
+		//switch文の中から何度呼び出されても一度だけ動作するようにするもの
 		this->start_s = start_s;
 		this->end_s = end_s;
-		end_potion[0] = now_p[0];//手動で動かした後に自動に切り替えた時のつじつま合わせ
-		end_potion[1] = now_p[1];
 		dis_[0] = dis_[0] - now_p[0];
 		dis_[1] = dis_[1] - now_p[1];
-		dis=sq(dis_[0]) + sq(dis_[1]);
+		dis = sq(dis_[0]) + sq(dis_[1]);
 		dis = sqrt(dis);//極座標でのrを求める
 		dir = atan2(dis_[1], dis_[0]);//返り値はラジアン
-		//座標から角度を求める
-		//おそらくatan2に式を入れてしまうと結果がバグる！！！！
+		//atan2は座標から角度を求める
+		//おそらくatan2に式を入れてしまうと結果がバグる
+
 		up_t = (max_s - start_s) * acc_; //最高速度までの加速にかかる時間 /s
 		down_t = (max_s - end_s) * acc_; //減速にかかる時間 /s
-		L1 = start_s * up_t + acc * sq(up_t) *0.5; //加速時における移動距離 /m
-		L3 = end_s * down_t + acc * sq(down_t)*0.5; //減速時における移動距離/m
-		if (L1 + L3 > abs(dis)) {
-			//disの台形ができなくなり、三角形になるときの制御
-			limit_s = (2.00 * acc * abs(dis) + sq(start_s) + sq(end_s)) *0.50;
-			limit_s= sqrt(limit_s);
-			up_t= (limit_s - start_s) *acc_;
-			down_t= (limit_s - end_s) * acc_;
+		L1 = start_s * up_t + acc * sq(up_t) * 0.5; //加速時における移動距離 /m
+		L3 = end_s * down_t + acc * sq(down_t) * 0.5; //減速時における移動距離/m
+		if (L1 + L3 > abs(dis)) {//台形ができなくなり、三角形になるときの制御
+			limit_s = (2.00 * acc * abs(dis) + sq(start_s) + sq(end_s)) * 0.50;
+			limit_s = sqrt(limit_s);
+			up_t = (limit_s - start_s) * acc_;
+			down_t = (limit_s - end_s) * acc_;
 			all_t = up_t + down_t;
 			max_power_t = 0.0;
 		}
 		else {//台形が作れる場合の制御 
 			max_power_t = (abs(dis) - L1 - L3) / max_s; //最高速度での移動時間 /s
 			all_t = up_t + down_t + max_power_t; //移動にかかる合計時間 / s
+			limit_s = max_s;
 		}
 		cal_state = true;
 	}
 
-	
-	void calculation(double start_s, double end_s, double radius,double angle, double dis_[2], double now_p[2]) {
-		//関数の多重定義だお
-		//角度の遷移を台形制御する
-		this->start_s = start_s;
-		this->end_s = end_s;
-		double cal_max_p_s = max_s / radius;//台形制御のときの最大速度（角速度）
-
-
-
-
-
-
-	}
-
 	void tar_point() {
 		//  X=Vot+(1/2)*at^2;
-		if (cal_state) timer_start();//タイマーを開始する　経路計算が終わった後だけ実行する
-		double t= read_ms() * 0.001;
+		if (cal_state) {
+		}
+		double t = read_ms() * 0.001;
 		double ut = constrain(t, 0.00, up_t);
-		double dt = constrain(t- (up_t + max_power_t), 0.00, down_t);
-		double target_ = acc * sq(ut) *0.50 + start_s * ut
+		double dt = constrain(t - (up_t + max_power_t), 0.00, down_t);
+		double target_ = acc * sq(ut) * 0.50 + start_s * ut
 			+ max_s * constrain(t - up_t, 0.00, max_power_t)
-			+ (acc * sq(down_t) * 0.50 - acc * sq(down_t - dt) * 0.50) + end_s * dt;
-		if (dis < 0)    target_ *= -1;
+			+  (-acc * sq(dt) * 0.50) + limit_s * dt;
+		//座標の更新をするところ
+		//毎周期ごとに積算せずに位置から計算
 		target[0] = target_ * cos(dir) + end_potion[0];
 		target[1] = target_ * sin(dir) + end_potion[1];
+
 		if (all_t <= t) {
-			end_potion [0] =target[0];//一つの経路を巡行し終えた時の座標の情報を保持
+			end_potion[0] = target[0];//一つの経路を巡行し終えた時の座標の情報を保持
 			end_potion[1] = target[1];
 			timer_stop();
-			timer_reset();
 			cal_state = false;
 			next_state = true;
 		}
@@ -147,7 +124,7 @@ public:
 	bool next_status() {//今回の経路を巡行し終えたらtrueになる
 		return next_state;
 	}
-	void set_next_status() {//次の経路をセットしたら必ず行うこと
+	void set_next_status() {//次の経路をセットしたら必ず行う
 		next_state = false;
 	}
 	double end_p_x() {
@@ -165,9 +142,5 @@ public:
 	double all_time() {
 		return all_t;
 	}
-	double up__tmie() {
-		return up_t;
-	}
 };
 extern Trapezoidal auto_set;
-
